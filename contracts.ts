4c999157a084938ef49eda8cfd812003e1cb9355
Luciano Bertenasco <lbertenasco@gmail.com>
2021-07-07 18:45:12 -0300

    feat: beta release

--- contracts.ts
+++ contracts.ts
@@ -1,8 +1,18 @@
-export const oracle = {
-  oracleBondedKeeper: '0xA8646cE5d983E996EbA22eb39e5956653ec63762',
-  partialKeep3rV1OracleJob: '0x5efD850044Ba76b8ffE49437CB301be3568bA696',
-  yearnKeep3rV2OracleFactory: '0xcEA967B693e89B1FFb08718c3106847616e95607',
-  keep3rV2OracleFactoryWrapper: '0xa07662C041C4F7B31Fd61C13C44212fF35b5296D',
-  yUnsafeOracleV1: '0xD1f5aEe3f025d2f4e42dd74DEC17d5FaA4707CbC',
-  uniswapV3OracleWrapper: '0x71bdC5Ca931Ca9C11C5d19375cA6C0aAe5F5eA5f',
+export const oracleBondedKeeper = {
+  mainnet: '0xA8646cE5d983E996EbA22eb39e5956653ec63762',
+};
+export const partialKeep3rV1OracleJob = {
+  mainnet: '0x5efD850044Ba76b8ffE49437CB301be3568bA696',
+};
+export const yearnKeep3rV2OracleFactory = {
+  mainnet: '0xcEA967B693e89B1FFb08718c3106847616e95607',
+};
+export const keep3rV2OracleFactoryWrapper = {
+  mainnet: '0xa07662C041C4F7B31Fd61C13C44212fF35b5296D',
+};
+export const yUnsafeOracleV1 = {
+  mainnet: '0xD1f5aEe3f025d2f4e42dd74DEC17d5FaA4707CbC',
+};
+export const uniswapV3OracleWrapper = {
+  mainnet: '0x71bdC5Ca931Ca9C11C5d19375cA6C0aAe5F5eA5f',
 };
